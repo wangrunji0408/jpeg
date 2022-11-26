9d3bbb02b3d34163725c4e6926bc999fddfbf0e5
Runji Wang <wangrunji0408@163.com>
2022-11-26 21:54:32 +0800

    optimize huffman encoding
    Signed-off-by: Runji Wang <wangrunji0408@163.com>

--- src/huffman.rs
+++ src/huffman.rs
@@ -1,6 +1,7 @@
 use super::Decoder;
 use num_enum::TryFromPrimitive;
 use std::{
+    collections::HashMap,
     fmt::Debug,
     io::{Error, ErrorKind, Read, Result},
     str::FromStr,
@@ -10,7 +11,7 @@ use tracing::debug;
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct HuffmanTable {
     pub class: HuffmanTableClass,
-    pub map: Vec<(Code, u8)>,
+    pub map: HashMap<Code, u8>,
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash, TryFromPrimitive)]
@@ -23,15 +24,35 @@ pub enum HuffmanTableClass {
     AC1 = 0x11,
 }
 
-#[derive(Default, Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
-pub struct Code {
-    pub value: u8,
-    pub len: u8,
+/// A Huffman code.
+///
+/// The inner value is the bit string as integer with an prefix 1.
+/// e.g. "011" => Code(0b1011)
+#[derive(Clone, Copy, PartialEq, Eq, PartialOrd, Ord, Hash)]
+pub struct Code(u16);
+
+impl Default for Code {
+    fn default() -> Self {
+        Code(1)
+    }
+}
+
+impl Code {
+    fn value(self) -> u16 {
+        if self.0.is_power_of_two() {
+            0
+        } else {
+            self.0 & !(self.0.next_power_of_two() >> 1)
+        }
+    }
+    fn len(self) -> u32 {
+        u16::BITS - 1 - self.0.leading_zeros()
+    }
 }
 
 impl Debug for Code {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:0len$b}", self.value, len = self.len as usize)
+        write!(f, "{:0len$b}", self.value(), len = self.len() as usize)
     }
 }
 
@@ -39,21 +60,18 @@ impl FromStr for Code {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self> {
-        Ok(Code {
-            value: u8::from_str_radix(s, 2)
-                .map_err(|_| Error::new(ErrorKind::InvalidData, "Invalid code"))?,
-            len: s.len() as u8,
-        })
+        let value = u16::from_str_radix(s, 2)
+            .map_err(|_| Error::new(ErrorKind::InvalidData, "Invalid code"))?;
+        Ok(Code(value | (1 << s.len() as u16)))
     }
 }
 
 impl Code {
     fn inc(&mut self) {
-        self.value += 1;
+        self.0 += 1;
     }
     fn double(&mut self) {
-        self.value <<= 1;
-        self.len += 1;
+        self.0 *= 2;
     }
 }
 
@@ -78,12 +96,13 @@ impl<R: Read> Decoder<R> {
             len -= 1 + 16;
 
             let mut code = Code::default();
-            let mut map = vec![];
-            for count in counts {
+            let mut map = HashMap::new();
+            assert_eq!(counts[15], 0, "not supported");
+            for &count in &counts[0..15] {
                 code.double();
                 for _ in 0..count {
                     let value = self.read_byte()?;
-                    map.push((code, value));
+                    map.insert(code, value);
                     code.inc();
                 }
                 len -= count as u16;
@@ -111,7 +130,7 @@ mod tests {
             dhts,
             vec![HuffmanTable {
                 class: HuffmanTableClass::DC0,
-                map: vec![
+                map: [
                     ("00".parse().unwrap(), 0),
                     ("010".parse().unwrap(), 1),
                     ("011".parse().unwrap(), 2),
@@ -123,8 +142,10 @@ mod tests {
                     ("111110".parse().unwrap(), 8),
                     ("1111110".parse().unwrap(), 9),
                     ("11111110".parse().unwrap(), 10),
-                    ("011111110".parse().unwrap(), 11)
+                    ("111111110".parse().unwrap(), 11),
                 ]
+                .into_iter()
+                .collect(),
             }]
         );
     }
